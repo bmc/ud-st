b05f66490a2a66f1befc7281b45011d17c5a44ed
Brian Clapper <bmc@clapper.org>
2021-09-10 15:10:43 -0400

    Commented code. Allow code to be viewed from web page.

--- code.js
+++ code.js
@@ -87,59 +87,77 @@ const NORMAL_TO_UPSIDE_DOWN = {
   'z': 'z'
 }
 
+/**
+ * Given the ID of an HTML <textarea> element, return the contents of the
+ * text box.
+ */
 function getTextAreaInput(elementId) {
   return document.getElementById(elementId).value;
 }
 
-function setTextAreaContent(elementId, text) {
+/**
+ * Given the ID of an HTML <textarea> element, set the contents of the
+ * text box to the specified string.
+ */
+ function setTextAreaContent(elementId, text) {
   document.getElementById(elementId).value = text;
 }
 
-function clearTextAreaContent(elementId) {
+/**
+ * Given the ID of an HTML <textarea> element, clear the contents of the
+ * text box.
+ */
+ function clearTextAreaContent(elementId) {
   setTextAreaContent(elementId, '');
 }
 
-function removeAllChildNodes(element) {
-  while (element.lastChild) {
-    element.removeChild(element.lastChild)
-  }
-}
-
-function replaceTextInside(elementId, newText) {
-  /*
-    Useful for replacing the text inside a <div> or <span>.
-  */
-  var target = document.getElementById(elementId);
-  removeAllChildNodes(target);
-  var newTextNode = document.createTextNode(newText);
-  target.appendChild(newTextNode);
-}
-
+/**
+ * Copy the specified string to the clipboard.
+ */
 function copyToClipboard(s) {
   navigator.clipboard.writeText(s)
 }
 
+/**
+ * Enable or disable a <button>.
+ *
+ * @param buttonId  the ID of the <button> element
+ * @param enabled   true to enable the button, or false to disable it
+ */
 function enableButton(buttonId, enabled) {
   document.getElementById(buttonId).disabled = !enabled;
 }
 
+/**
+ * Given a string, return the upside-down version. The mappings from regular
+ * to upside-down version is the NORMAL_TO_UPSIDE_DOWN object, above. Any
+ * character that does not have a mapping is stored as is.
+ */
 function flipUpsideDown(s) {
-  function lookup(c) {
-    return NORMAL_TO_UPSIDE_DOWN[c] || c;
-  }
-  return s.split('').reverse().map(lookup).join('');
+  return s.split('')
+          .reverse()
+          .map(c => NORMAL_TO_UPSIDE_DOWN[c] || c)
+          .join('');
 }
 
 // Store the upside down characters here. Beats pulling them back out
 // of the DOM to copy them to the clipboard.
 var upsideDownBuf = '';
 
+/**
+ * Enable or disable the upside-down Copy and Clear buttons, based on
+ * whether we have text in the input box or not.
+ */
 function checkUpsideDownButtons() {
   var nonEmpty =  upsideDownBuf.length > 0;
   enableButton("copy-upside-down-text", nonEmpty);
   enableButton("clear-upside-down-text", nonEmpty);
 }
 
+/**
+ * Handle a change in the contents of the upside-down input <textarea>.
+ * This function will change the output text box and update the buttons.
+ */
 function upsideDownInputChanged() {
   var value = getTextAreaInput("upside-down-input");
   upsideDownBuf = flipUpsideDown(value);
@@ -147,10 +165,20 @@ function upsideDownInputChanged() {
   checkUpsideDownButtons();
 }
 
+/**
+ * Invoked when the upside-down Copy button is clicked, this function
+ * copies the contents of the output text box (really, the upsideDownBuf)
+ * to the clipboard.
+ */
 function copyUpsideDown() {
   copyToClipboard(upsideDownBuf);
 }
 
+/**
+ * Invoked when the upside-down Clear button is clicked, this function
+ * clears the contents the input and output text boxes, clears the
+ * corresponding buffer, and disables the associated Copy and Clear buttons.
+ */
 function clearUpsideDown() {
   clearTextAreaContent("upside-down-input");
   clearTextAreaContent("upside-down-output");
@@ -158,20 +186,34 @@ function clearUpsideDown() {
   checkUpsideDownButtons();
 }
 
-// Store the upside down characters here. Beats pulling them back out
+// Store the strikethrough characters here. Beats pulling them back out
 // of the DOM to copy them to the clipboard.
 var strikethroughBuf = '';
 
+/**
+ * Given a string, return a version with each character stricken out.
+ * This function relies on Unicode combining character U+0336, the
+ * COMBINING LONG STROKE OVERLAY character. Placing this character after
+ * a "normal" character produces the strikeout version.
+ */
 function strikeout(s) {
   return s.split('').map(c => `${c}\u0336`).join('');
 }
 
-function checkStrikethroughButtons() {
+/**
+ * Enable or disable the strikethrough Copy and Clear buttons, based on
+ * whether we have text in the input box or not.
+ */
+ function checkStrikethroughButtons() {
   var nonEmpty =  strikethroughBuf.length > 0;
   enableButton("copy-strikethrough-text", nonEmpty);
   enableButton("clear-strikethrough-text", nonEmpty);
 }
 
+/**
+ * Handle a change in the contents of the strikethrough input <textarea>.
+ * This function will change the output text box and update the buttons.
+ */
 function strikethroughInputChanged() {
   var value = getTextAreaInput("strikethrough-input");
   strikethroughBuf = strikeout(value);
@@ -179,10 +221,20 @@ function strikethroughInputChanged() {
   checkStrikethroughButtons();
 }
 
-function copyStrikethrough() {
+/**
+ * Invoked when the strikethrough Copy button is clicked, this function
+ * copies the contents of the output text box (really, the strikethroughBuf)
+ * to the clipboard.
+ */
+ function copyStrikethrough() {
   copyToClipboard(strikethroughBuf);
 }
 
+/**
+ * Invoked when the strikethrough Clear button is clicked, this function
+ * clears the contents the input and output text boxes, clears the
+ * corresponding buffer, and disables the associated Copy and Clear buttons.
+ */
 function clearStrikethrough() {
   clearTextAreaContent("strikethrough-input");
   clearTextAreaContent("strikethrough-output");
